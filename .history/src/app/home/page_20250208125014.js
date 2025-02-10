fd21e8dd88877e18a9a2c4b86fdba9fbe242d6b8
ujjwalkathait <kathaitujjwal@gmail.com>
2025-02-10 12:57:41 +0530

    contact page

--- .history/src/app/home/page_20250208125014.js
+++ .history/src/app/home/page_20250208125014.js
@@ -36,7 +36,7 @@ const page = () => {
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
             <Image
-            src="./globe.svg"
+            src="./file.svg"
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
